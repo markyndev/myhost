40bcf19f98b026e7cdde0bd1cb71012192c55315
Marcus Vinicius <markynrodrisilva@gmail.com>
2022-01-31 23:03:03 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,53 +1,25 @@
 //FrameWorks
-
-const low = require('lowdb')
 const express = require("express");
-const app = express();
-const handlebars = require("express-handlebars");
-const bodyParser = require("body-parser")
-let status = 0
-let cloud = undefined
-// Configurações de renderização 
-var handle = handlebars.create({
-    defaultLayout: 'main'
+const { dirname } = require("path/posix");
+const server = express();
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  console.log("MyHost 1.0 - Your own PaaS cloud service")
+  console.log("developed by @markyndev")
+  console.log("")
+  readline.question('Enter the IP you will use: ', input_ip => {
+    readline.question('Enter the PORT you will use: ', input_port => {
+        console.log("# If it's your first time using it, put your web files in the ./public folder")
+       server.listen(input_port, input_ip);
+       server.use(express.static('./public'));
+
+        console.log(`Server is running on: http://${input_ip}:${input_port}`)
+    });
 });
-app.use(express.static('public'));
-app.use('/images', express.static('images'));
-app.engine('handlebars', handle.engine);
-app.set('view engine', 'handlebars');
-// Configurações BodyParser
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
 
 // Rotas
 
-app.get('/MHPanel/config', function (req, res) {
-
-    if(cloud === undefined){
-        console.log(`[MyHost] Apparently you haven't configured your cloud yet.`)
-    res.render('./config')
-    app.post('/MHPanel/config', function (req, res) {
-        let receber = req.body
-        cloud = `http://${receber.ip}:${receber.port}`
-        app.listen(receber.port, receber.ip);
-
-        app.use(express.static('./public'));
-        console.log(`-`)
-         console.log(`[MyHost] Your cloud is running on http://${receber.ip}:${receber.port}`)
-         res.render('./alert',{errorname:"You are successfully hosting a website!",errordesc:`To be able to access on other devices on the network it may be necessary to allow connections from "NodeJS" in your firewall.`,button:`Go to ${cloud}`,action: `location.href = '${cloud}';`})
-        
-    });
-}else{
-    console.log(cloud)
-   console.log(`[MyHost] ERROR: You've already set up your cloud... CLOSING MyHost`)
-   res.render('./alert',{errorname:"ERROR: You've already set up your cloud",errordesc:`Just once you can configure your cloud domain, port and folder. \n If you want to reconfigure it is necessary to restart MyHost`,button:`Go to ${cloud}`,action: `location.href = '${cloud}';`})
-
-} 
-})
-
-// Respostas servidor
-const port = 80;
-console.log("Welcome to MyHost - Share your web pages easily and quickly over the network")
-console.log("")
-var cfg_server = app.listen(process.env.PORT || port, () => console.log(`[MyHost] first config the cloud: http://localhost/MHPanel/config`));
-
+//const port = 3000;
+//server.listen(process.env.PORT || port, () => console.log(`Running http://localhost:${port}`));
